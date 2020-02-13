819def8fb0a0ad6d5404e322ca4df1c2fce45424
Melinda Deinert <melinda@unabridgedsoftware.com>
2020-02-12 19:10:38 -0600

    convert repo to use eleventy

---
+++ .eleventy.js
@@ -0,0 +1,11 @@
+module.exports = function(eleventyConfig) {
+  // Layouts
+  // Add layout names here to enable referencing them by just their file name
+  // instead of the entire path
+  eleventyConfig.addLayoutAlias('default', `layouts/default.liquid`);
+  // eleventyConfig.addLayoutAlias('mission-faq', `layouts/mission-faq.liquid`);
+
+  // Passthrough copy
+  // Just copy this content into the _site directory
+  eleventyConfig.addPassthroughCopy({'assets': 'assets' });
+};
