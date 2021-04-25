3cfa7e7ebedaed63e1af5de2dd4d124e8cdcedc5
Gwendolyn Holley (Wendy) <Gmfholley@gmail.com>
2021-04-25 18:50:42 -0500

    Revert "Add Share Omaha banner and link (#100)" (#101)
    This reverts commit 6f194cff6b5559267d80fb8c2448241ce753ef44.

--- assets/scripts/mission.js
+++ assets/scripts/mission.js
@@ -4,7 +4,7 @@ window.onscroll = function() {
     var currentScrollPos = window.pageYOffset;
     if (prevScrollpos > currentScrollPos) {
       // 40px fudged up so background does not even slightly show through sections
-      document.getElementById("mission").style.top = "120px";
+      document.getElementById("mission").style.top = "39px";
     } else if (currentScrollPos > 40) {
       document.getElementById("mission").style.top = "-70px";
     }
